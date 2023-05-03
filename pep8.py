9f326613367e6a0112bbac9c69384f12e8a05ff9
Juanito <juan.manuel.chavez.peralta@uabc.edu.mx>
2023-05-02 19:53:15 -0700

    Aplique pep8 al archivo

--- pep8.py
+++ pep8.py
@@ -1,10 +1,18 @@
-class User():
+#CamelCase
+class UserAdmin():
 
-    def __init__(self, username, password=''):
+
+    # Identacion: 4 Espacion (no tabs)
+    # Parametros separados por , 1 espacip
+
+
+    def __init__(self, username, password = ''):
         self.username = username
         self.password = password
+
 
     def set_password(self):
         pass
 
-    codyuser = user ('cody')
+#snake_case
+cody_user = UserAdmin('cody')  
